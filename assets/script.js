9dc8dfe5b5e66964e50469f58ad4881750f13321
ItsGeeBee <gracelbrown@hotmail.com>
2022-06-08 20:15:53 +1000

    variables for critera promopts and 'if' loop made

--- assets/script.js
+++ assets/script.js
@@ -23,4 +23,31 @@ function generatePassword() {
         alert("Password length must be between 8-128 characters!");
         var lengthRange = (prompt("Please pick a password length between 8 - 128 characters?"));
         } 
+    // Window prompts for user to click 'ok' or cancel - Decides what the password will be made up with //
+    var includeSpecialChar = confirm("Would like to include special characters");
+    var includeNumber = confirm("Would like to include numeric characters");    
+    var includeLowercase = confirm("Would like to include lowercase characters");
+    var includeUpperCase = confirm("Would like to include uppercase characters");
+
+    // empty array //
+      var passwordCharacters = []
+      
+    // Loop, checking what needs to be included in the password //
+    if (includeSpecialChar) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
     }
+
+    if (includeNumber) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+      
+    if (includeLowercase) {
+      passwordCharacters = passwordCharacters.concat(charLower)
+    }
+
+    if (includeUpperCase) {
+      passwordCharacters = passwordCharacters.concat(charUpper)
+    }
+
+      console.log(passwordCharacters)
+}
