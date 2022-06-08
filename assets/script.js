9772028cc2c7434586c59e00e2d965ba53c1bf6c
ItsGeeBee <gracelbrown@hotmail.com>
2022-06-08 13:49:45 +1000

    Determined what variables are needed

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//Assignment Code //
+var generateBtn = document.querySelector("#generate");
+
+// Global variables //
+var lengthRange = "";
+var includeSpecialChar;
+var includeNumber;
+var includeUpperCase;
+var includeLowercase;
+
+// set arrays for each //
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
